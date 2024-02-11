dc8e04cf3957c8fa14ce83af75e5c8700d995f5d
sfcampbell <sfcampbell@gmail.com>
2024-02-10 23:38:53 -0700

    issue with main.ts port variable

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -10,10 +10,10 @@ import { SVC_PORT } from './config';
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonOptions);
   const app = await NestFactory.create(AppModule, { logger });
-  const port = SVC_PORT.port
+  const port = SVC_PORT.port;
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   app.use('/jobs', bullBoardMiddleware);
-  await app.listen({port});
+  await app.listen(port);
 }
 bootstrap();
