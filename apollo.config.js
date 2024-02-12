6b6675c02396ba1676dc0e6fed385e2ae1f18e36
sfcampbell <sfcampbell@gmail.com>
2024-02-12 12:23:37 -0700

    issue with web port variables

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,15 @@
 /* eslint import/no-commonjs: off */
 
+const port = process.env.PORT
+const apiURL = process.env.WEB_UI_API_URL || `${host}`;
+
 module.exports = {
   client: {
+    env: { API_URL: process.env.WEB_UI_API_URL }, 
+    env: { API_PORT: process.env.PORT},
     service: {
       name: 'bobarr',
-      url: 'http://localhost:${PORT}/graphql',
+      url: 'http://${API_URL}:${API_PORT}/graphql',
     },
     excludes: ['**/*.{ts,tsx,js,jsx}'],
   },
