3b2c39265a610db871ba1cf269803916fc0f514a
Campbell <sfcampbell@gmail.com>
2024-02-14 16:36:34 -0700

    adapted .yamls

--- apollo.config.js
+++ apollo.config.js
@@ -1,17 +1,20 @@
 /* eslint import/no-commonjs: off */
 
 module.exports = process.env.WEB_UI_API_URL ? {
+  env: { WEB_UI_API_URL: process.env.WEB_UI_API_URL },
+  env: { API_QUALITY: process.env.API_QUALITY },
+  env: { API_PORT: process.env.PORT },
   client: {
     service: {
-      name: 'bobarr',
-      url: [process.env.WEB_UI_API_URL,'/graphql'].join(''),
+      name: 'bobarr-' & { API_QUALITY },
+      url: { WEB_UI_API_URL} +'/graphql',
     },
     excludes: ['**/*.{ts,tsx,js,jsx}'],
   },
 }:{
   client: {
     service: {
-      name: 'bobarr',
+      name: 'bobarr-' & { API_QUALITY },
       url: 'http://localhost:4000/graphql',
     },
     excludes: ['**/*.{ts,tsx,js,jsx}'],
